acf5bd0dd18b500ae1721aa914f561953c5b5730
Bagwell <tbagwe0460@greenville.k12.sc.us>
2017-09-14 09:25:00 -0400

    Made a limit to the guesses.

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,14 +1,18 @@
 import random
 
+#Configuration
 low = 1
-high = 2000
+high = 100
+limit = 10
 
+#Start criteria
 rand = random.randrange(low, high)
 print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
 
 guess = -1
+tries = 0
 
-while guess != rand:
+while guess != rand and tries < limit:
     guess = input("Take a guess: ")
     guess = int(guess)
     
@@ -16,7 +20,11 @@ while guess != rand:
         print("You guessed too low.")
     elif guess > rand:
         print("You guessed too high.")
-    else:
-        print("You got it!")
 
-print("Game over")
+    tries += 1
+
+#Gives outcome for player
+if guess == rand:
+    print("Noice job m8.")
+else:
+    print("Git gud lmao. The number I was thinking of was " + str(rand) + " you scrub.")
