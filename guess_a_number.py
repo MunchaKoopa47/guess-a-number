2c2dd1685d7dda7ec62c713423ae5932c7317615
Bagwell <tbagwe0460@greenville.k12.sc.us>
2017-09-22 09:24:35 -0400

    Made some functionality tweaks.

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,41 +1,94 @@
 import random
+import math
 
-#Configuration
+# config
 low = 1
 high = 100
-limit = 10
+limit = round(math.log(high-low+1,2))
 
-#Start criteria
-rand = random.randrange(low, high)
-print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
-
-guess = -1
-tries = 0
-
-#Helper functions
+# helper functions
+def show_start_screen():
+    print("▄█░ ▀█░█▀ ▄█░ 　 ▒█▀▄▀█ █▀▀ 　 ▀▀█▀▀ █░░█ █▀▀ 　 █▀▀▀ █▀▀█ █▀▄▀█ █▀▀ ░ ")
+    print("░█░ ░█▄█░ ░█░ 　 ▒█▒█▒█ █▀▀ 　 ░░█░░ █▀▀█ █▀▀ 　 █░▀█ █▄▄█ █░▀░█ █▀▀ ▄ ")
+    print("▄█▄ ░░▀░░ ▄█▄ 　 ▒█░░▒█ ▀▀▀ 　 ░░▀░░ ▀░░▀ ▀▀▀ 　 ▀▀▀▀ ▀░░▀ ▀░░░▀ ▀▀▀ █ ")
+    print()
+    print("You get " + str(limit) + " tries to guess.")
+    print()
+def show_credits():
+    print()
+    print("▒█▀▀█ █▀▀█ █▀▀ █▀▀█ ▀▀█▀▀ █▀▀ █▀▀▄ 　 █▀▀▄ █░░█")
+    print("▒█░░░ █▄▄▀ █▀▀ █▄▄█ ░░█░░ █▀▀ █░░█ 　 █▀▀▄ █▄▄█")
+    print("▒█▄▄█ ▀░▀▀ ▀▀▀ ▀░░▀ ░░▀░░ ▀▀▀ ▀▀▀░ 　 ▀▀▀░ ▄▄▄█")
+    print()
+    print("▒█▀▄▀█ ░▀░ █▀▀▄ █▀▀ █▀▀ █▀▀█ █▀▀█ █▀▀ ▀▀█▀▀ █░█ ░▀░ █▀▀▄ ▄█░ █▀█ █▀▀█") 
+    print("▒█▒█▒█ ▀█▀ █░░█ █▀▀ █░░ █▄▄▀ █▄▄█ █▀▀ ░░█░░ █▀▄ ▀█▀ █░░█ ░█░ ░▄▀ ░░▀▄") 
+    print("▒█░░▒█ ▀▀▀ ▀░░▀ ▀▀▀ ▀▀▀ ▀░▀▀ ▀░░▀ ▀░░ ░░▀░░ ▀░▀ ▀▀▀ ▀▀▀░ ▄█▄ █▄▄ █▄▄█")
+    print("AKA ur mom")
+    print("AKA Mr. Clean")
+    print("AKA Tristan Bagwell.")
+    
 def get_guess():
     while True:
-        g = input("Take a guess: ")
+        guess = input("Guess a number: ")
 
-        if g.isnumeric():
-            g = int(g)
-            return g
+        if guess.isnumeric():
+            guess = int(guess)
+            return guess
         else:
-            print("You must enter a number you schweinhun!")
-            
-#Play game
-while guess != rand and tries < limit:
-    guess = get_guess()
-    
+            print("U must enter a number ya nub.")
+
+def pick_number():
+    print("I'm thinking of a number from " + str(low) + " to " + str(high) +".")
+
+    return random.randint(low, high)
+
+def check_guess(guess, rand):
     if guess < rand:
         print("You guessed too low.")
     elif guess > rand:
         print("You guessed too high.")
 
-    tries += 1
+def show_result(guess, rand):
+    if guess == rand:
+        print("Noice job m8!")
+    else:
+        print("Git gud lmao. Ur a skrub. The number was " + str(rand) + ".")
 
-#Gives outcome for player
-if guess == rand:
-    print("Noice job m8.")
-else:
-    print("Git gud lmao. The number I was thinking of was " + str(rand) + " you scrub.")
+def play_again():
+    while True:
+        decision = input("Wanna 1v1 me again? Insert credit to continue.")
+
+        decision = decision.lower()
+
+        if decision == 'credit':
+            return True
+        elif decision == 'n' or decision == 'no':
+            return False
+        else:
+            print("I don't understand. Please enter 'credit' or 'n' or 'no'.")
+
+def play():
+    guess = -1
+    tries = 0
+
+    rand = pick_number()
+    
+    while guess != rand and tries < limit:
+        guess = get_guess()
+        check_guess(guess, rand)
+
+        tries += 1
+
+    show_result(guess, rand)
+
+
+# Game starts running here
+show_start_screen()
+
+playing = True
+
+while playing:
+    play()
+    playing = play_again()
+
+show_credits()
